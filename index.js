734bbe990cc3bd5b9780a0ddcd28dddac97f2f66
Jacob Nobili <jnobili14@gmail.com>
2025-08-08 05:29:08 -0600

    Automatic Tax

--- index.js
+++ index.js
@@ -304,6 +304,11 @@ app.post('/create-checkout-session', async (req, res) => {
     try {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'], line_items: lineItems, mode: 'payment',
+			
+            automatic_tax: {
+                enabled: true,
+            },			
+			
             success_url: `${frontendUrl}/receipt.html?session_id={CHECKOUT_SESSION_ID}`,
             cancel_url: `${frontendUrl}/cancel.html`,
             shipping_address_collection: { allowed_countries: ['US'] },
